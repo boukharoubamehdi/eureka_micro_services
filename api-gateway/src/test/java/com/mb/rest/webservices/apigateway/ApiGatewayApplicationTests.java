348ceb66dad7a33ca58681126fd6540db2c576f0
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2023-01-09 19:45:17 -0500

    adding api-gateway (#3)

---
+++ api-gateway/src/test/java/com/mb/rest/webservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mb.rest.webservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
